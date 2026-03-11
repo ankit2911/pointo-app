393e89a1838cd6361594ad1388906b4ec6f869cd
Ankit Agrawal <ankit.agrawal2911@gmail.com>
2026-03-11 20:05:12 +0530

    minor fix

--- src/context/LanguageContext.tsx
+++ src/context/LanguageContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState, useEffect, ReactNode } from 'react';
+import React, { createContext, useContext, useState, ReactNode } from 'react';
 import { en, TranslationKey } from '../i18n/locales/en';
 import { hi } from '../i18n/locales/hi';
 
